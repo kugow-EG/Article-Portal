841c17b220ea057f663068c66ca9d5b1c2d22cbc
kugow <kugow@AUTOEKDY2ZWE4JN>
2025-01-11 14:09:59 +0530

    Initial Commit

---
+++ Model/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace ArticlePortal.Model
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string FullText { get; set; }
+        
+        
+    }
+}
---
+++ Services/FetchDataServices.cs
@@ -0,0 +1,36 @@
+﻿using ArticlePortal.Model;
+using System.Net.Http.Json;
+
+public class FetchDataServices
+{
+    private readonly HttpClient _httpClient;
+
+    public FetchDataServices(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Article>> GetArticlesAsync()
+    {
+        try
+        {
+            return await _httpClient.GetFromJsonAsync<List<Article>>("https://ps-dev-1-partnergateway.patientsky.dev/assignment/articles");
+        }
+        catch (Exception)
+        {
+            throw new Exception("Failed to fetch articles.");
+        }
+    }
+
+    public async Task<Article> GetArticleByIdAsync(int id)
+    {
+        try
+        {
+            return await _httpClient.GetFromJsonAsync<Article>($"https://ps-dev-1-partnergateway.patientsky.dev/assignment/articles/{id}");
+        }
+        catch (Exception)
+        {
+            throw new Exception("Failed to fetch the article details.");
+        }
+    }
+}
